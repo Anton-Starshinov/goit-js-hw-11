80ffce03d0601f239a49441484e503ceff0870a5
Anton-Starshinov <97900625+Anton-Starshinov@users.noreply.github.com>
2022-08-28 20:05:35 +0300

    add css

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const BASE_URL = 'https://pixabay.com/api/?key=';
+const API_KAY = '29562775-0cedab5e27dd705c115fa7ca4';
